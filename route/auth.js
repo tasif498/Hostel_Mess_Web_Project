5b2bca92d0f9fc321beb6668207aff09c4bb2eee
mzaeemnasir <m.zaeem.nasir@gmail.com>
2021-12-29 23:44:42 +0500

    added Login

--- route/auth.js
+++ route/auth.js
@@ -5,6 +5,7 @@ router.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Register User Router
 router.post("/register", async (req, res) => {
   const { name, email, password, phoneNumber } = req.body;
 
@@ -23,6 +24,28 @@ router.post("/register", async (req, res) => {
     console.log(
       `User => ${name}  using Email => ${email}Registered Successfully`
     );
+  } catch (err) {
+    console.log(err);
+  }
+});
+// Login User Router
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .json({ error: "Please Fill are the Required Fields" });
+  }
+  console.log(email, password);
+  try {
+    const user = await Users.findOne({ email: email });
+    if (!user) {
+      return res.status(422).json({ error: "Invalid Credentials" });
+    }
+    if (user.password !== password) {
+      return res.status(422).json({ error: "Invalid Credentials" });
+    }
+    res.status(201).json({ message: "User Logged In Successfully" });
   } catch (err) {
     console.log(err);
   }
